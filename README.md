fb4d74b8384fd0f4b54db50633c20611e1a08951
Sam Curren <telegramsam@gmail.com>
2019-06-18 14:05:05 -0600

    electronvue initial
    Signed-off-by: Sam Curren <telegramsam@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-aries-toolbox
+# aries-toolbox
+
+> Tools for working with Aries Agents
+
+#### Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:9080
+npm run dev
+
+# build electron application for production
+npm run build
+
+
+```
+
+---
+
+This project was generated with [electron-vue](https://github.com/SimulatedGREG/electron-vue) using [vue-cli](https://github.com/vuejs/vue-cli). Documentation about the original structure can be found [here](https://simulatedgreg.gitbooks.io/electron-vue/content/index.html).
---
+++ src/renderer/components/ConnectionList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="wrapper" class="container-fluid">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Agent Toolkit</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">New Connection</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="card" style="">
+      <div class="card-body">
+        <h5 class="card-title">Alice Agent</h5>
+        <a href="#" class="card-link">Connect</a>
+        <a href="#" class="card-link">Edit</a>
+      </div>
+    </div>
+    <div class="card" style="">
+      <div class="card-body">
+        <h5 class="card-title">Bob Agent</h5>
+        <a href="#" class="card-link">Connect</a>
+        <a href="#" class="card-link">Edit</a>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'connection-list',
+    components: {  },
+    methods: {
+
+    }
+  }
+</script>
+
+<style>
+</style>
