189104cd90ec0d3e71214a82187d9d12f3f5b7c4
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-10-28 11:00:12 -0400

    Clarifications and guardrails in message_bus.js
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- src/renderer/message_bus.js
+++ src/renderer/message_bus.js
@@ -1,13 +1,23 @@
+/* Message Bus Vue Plugin
+ * ======================
+ * This Vue plugin adds `$message_bus` attributes to Vue components specifying
+ * the `message_bus` option. Values of this option can either be `source` or
+ * `derive`. `source` will create a new message bus and make that component the
+ * primary holder of the message bus. `derive` will look in the components
+ * parent heirarchy for a defined message bus and use the first found message
+ * bus.
+ */
+
 import Vue from 'vue';
 const uuidv4 = require('uuid/v4');
 
 export default function() {
-    Vue.message_bus = {};
+    Vue.message_bus_directory = {};
     Vue.getMessageBus = function(key) {
-        if (!(key in Vue.message_bus)) {
-            Vue.message_bus[key] = new Vue();
+        if (!(key in Vue.message_bus_directory)) {
+            Vue.message_bus_directory[key] = new Vue();
         }
-        return Vue.message_bus[key];
+        return Vue.message_bus_directory[key];
     };
     Vue.mixin({
         beforeCreate: function() {
@@ -15,7 +25,8 @@ export default function() {
             if (options.message_bus) {
                 if (options.message_bus === 'source') {
                     this.$message_bus = Vue.getMessageBus(uuidv4().toString());
-                } else if (options.message_bus == 'derive') {
+                } else if (options.message_bus === 'derive') {
+                    // Recursively find a message bus in parent heirarchy.
                     function derive(component) {
                         if (component.$message_bus) {
                             console.log('Found message bus on component:', component);
@@ -34,6 +45,16 @@ export default function() {
                 } else {
                     console.error('Unrecognized message bus option:', options.message_bus);
                 }
+            } else {
+                // Gaurdrails to assist agent component developers.
+                this.$message_bus = {
+                    $on: function () {
+                        console.error('The `message_bus` option on this component was not specified. Add `message_bus: "[source|derive],"` to your options.')
+                    },
+                    $emit: function() {
+                        console.error('The `message_bus` option on this component was not specified. Add `message_bus: "[source|derive],"` to your options.')
+                    }
+                };
             }
         }
     });
