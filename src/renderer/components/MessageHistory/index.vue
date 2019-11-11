0eb87be7182b43224cb36810c04812a95ee53c36
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-11-11 12:56:00 -0500

    Convert message history
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- src/renderer/components/MessageHistory/index.vue
+++ src/renderer/components/MessageHistory/index.vue
@@ -1,7 +1,7 @@
 <template>
   <el-row>
-    <el-button type="secondary" @click="history.slice(o, history.length)">Clear</el-button>
-    <div class="message-display" v-for="m in history.slice().reverse()" :key="m.msg['@id']">
+    <el-button type="secondary" @click="message_history.slice(o, message_history.length)">Clear</el-button>
+    <div class="message-display" v-for="m in message_history.slice().reverse()" :key="m.msg['@id']">
       <i>{{m.direction}}</i>
       <vue-json-pretty
         :deep=1
@@ -12,30 +12,34 @@
 </template>
 
 <script>
-import message_bus from '../../message_bus.js';
+import message_bus from '@/message_bus.js';
+import share from '@/share.js';
 import VueJsonPretty from 'vue-json-pretty';
+
+export const shared = {
+  data: {
+    message_history: []
+  },
+  listeners: {
+    'message-received': (share, msg) => {
+      share.message_history.push({msg: msg, direction: 'Received'});
+    },
+    'send-message': (share, msg) => {
+      share.message_history.push({msg: msg, direction: 'Sent'});
+    }
+  }
+};
 
 export default {
   name: 'message-history',
-  mixins: [message_bus()],
+  mixins: [
+    message_bus(),
+    share({
+      use: ['message_history']
+    })
+  ],
   components: {
     VueJsonPretty
   },
-  data: function() {
-    return {
-      history: []
-    }
-  },
-  created: function() {
-    let component = this;
-    this.$message_bus.$on('message-received', (msg) => component.history.push({
-      'msg': msg,
-      'direction': 'Received'
-    }));
-    this.$message_bus.$on('send-message', (msg) => component.history.push({
-      'msg': msg,
-      'direction': 'Sent'
-    }));
-  }
 }
 </script>
