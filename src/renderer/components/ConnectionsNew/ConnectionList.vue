16961e6a243d828966a3e0258c17923b7c5dbede
Daniel Bluhm <daniel.bluhm@sovrin.org>
2020-03-04 15:02:31 -0500

    Add ConnectionsNew
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

---
+++ src/renderer/components/ConnectionsNew/ConnectionList.vue
@@ -0,0 +1,98 @@
+<template >
+  <div v-if="list.length">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">{{ title }}</a>
+      <el-button
+        type="primary"
+        icon="el-icon-refresh"
+        @click="$emit('refresh',)"></el-button>
+    </nav>
+    <el-collapse v-model="expanded_items">
+      <ul class="list">
+        <el-collapse-item
+          v-for="(connection) in list"
+          v-bind:title="get_name(connection)"
+          :name="connection.connection_id"
+          :key="title+connection.connection_id">
+          <el-row>
+            <div>
+              <vue-json-pretty
+                :deep=1
+                :data="connection">
+              </vue-json-pretty>
+            </div>
+            <template v-if="editable">
+              <el-button @click="edit(connection)">Edit</el-button>
+            </template>
+            <el-button type="danger" @click="delete_conn(connection)">Delete</el-button>
+            <el-button v-on:click="collapse_expanded(connection)">^</el-button>
+          </el-row>
+        </el-collapse-item>
+      </ul>
+    </el-collapse>
+    <el-dialog title="Edit Connection" :visible.sync="editFormActive">
+      <el-form :model="editForm">
+        <el-form-item label="Role:" :label-width="formLabelWidth">
+          <el-input v-model="editForm.role" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Label:" :label-width="formLabelWidth">
+          <el-input v-model="editForm.label" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editFormActive = false">Cancel</el-button>
+        <el-button type="primary" @click="update">Confirm</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import VueJsonPretty from 'vue-json-pretty';
+
+export default {
+  name: 'connection-list',
+  props: ['title', 'list','editable'],
+  components: {
+    VueJsonPretty,
+  },
+  data () {
+    return {
+      expanded_items:[],
+      editFormActive: false,
+      editForm: {
+        connection_id: '',
+        role: '',
+        label: '',
+      },
+      formLabelWidth: '100px'
+    }
+  },
+  methods: {
+    get_name: function(connection) {
+      if('label' in connection) {
+        return connection.label;
+      };
+      return connection.connection_id;
+    },
+    edit: function (connection) {
+      this.editForm.connection_id = connection.connection_id;
+      this.editForm.role = connection.their_role;
+      this.editForm.label = connection.label;
+      this.editFormActive = true;
+    },
+    update: function() {
+      this.editFormActive = false;
+      this.$emit('connection-editted', this.editForm);
+    },
+    delete_conn: function (connection) {
+      this.$emit('connection-deleted', connection);
+    },
+    collapse_expanded: function(connection){
+      this.expanded_items = this.expanded_items.filter(
+        item => item != connection.connection_id
+      );
+    },
+  }
+}
+</script>
---
+++ src/renderer/components/ConnectionsNew/index.vue
@@ -0,0 +1,172 @@
+<template>
+  <el-row>
+    <connection-list
+      title="Active Connections:"
+      editable="true"
+      :list="active_connections_new"
+      @connection-editted="update_connection"
+      @connection-deleted="delete_connection"
+      @refresh="fetch_connections_new"></connection-list>
+    <connection-list
+      title="Pending Connections:"
+      editable="true"
+      :list="pending_connections"
+      @connection-editted="update_connection"
+      @connection-deleted="delete_connection"></connection-list>
+    <connection-list
+      title="Failed Connections:"
+      editable="false"
+      :list="failed_connections"
+      @connection-editted="update_connection"
+      @connection-deleted="delete_connection"></connection-list>
+
+    <p>Add connection from invitation:</p>
+    <el-form @submit.native.prevent>
+      <el-form-item
+        label="Invitation URL:">
+        <el-input
+          style="width: 300px;"
+          v-model="invitation">
+          <el-button
+            slot="append"
+            type="primary"
+            icon="el-icon-plus"
+            @click="recieve_invitation">Add</el-button>
+        </el-input>
+      </el-form-item>
+    </el-form>
+  </el-row>
+</template>
+
+<script>
+import ConnectionList from './ConnectionList.vue';
+import share from '@/share.js';
+import message_bus from '@/message_bus.js';
+
+export const protocol = 'https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-connections/0.1';
+export const metadata = {
+  menu: {
+    label: 'Connections [New]',
+    icon: 'el-icon-user',
+    group: 'Agent to Agent',
+    priority: 30,
+    required_protocols: [
+      protocol
+    ]
+  },
+  types: {
+    connection: protocol + '/connection',
+    get_list: protocol + '/get-list',
+    list: protocol + '/list',
+    update: protocol + '/update',
+    delete: protocol + '/delete',
+    deleted: protocol + '/deleted',
+    receive_invitation: protocol + '/receive-invitation'
+  }
+};
+
+export const shared = {
+  data: {
+    connections_new: []
+  },
+  computed: {
+    active_connections_new: function() {
+        return Object.values(this.connections_new).filter(
+          conn => {
+            if (!("state" in conn)) {
+              return false;
+            }
+            return conn.state === "active"
+          }
+        );
+    },
+    id_to_connection_new: function(connection_id) {
+      let map = {};
+      this.connections_new.forEach((connection) => {
+        map[connection.connection_id] = connection;
+      })
+      return map;
+    }
+  },
+  listeners: {
+    [metadata.types.list]:
+    (share, msg) => share.connections_new = msg.connections,
+    [metadata.types.connection]:
+    (share, msg) => share.fetch_connections_new(),
+    [metadata.types.deleted]:
+    (share, msg) => share.fetch_connections_new(),
+  },
+  methods: {
+    fetch_connections_new: ({send}) => {
+      send({
+        "@type": metadata.types.get_list,
+      });
+    }
+  }
+};
+
+export default {
+  name: 'connections-new',
+  components: {
+    ConnectionList
+  },
+  mixins: [
+    message_bus(),
+    share({
+      use: ['connections_new', 'active_connections_new'],
+      actions: ['fetch_connections_new']
+    })
+  ],
+  data: function() {
+    return {
+      'invitation': '',
+    }
+  },
+  created: async function() {
+    await this.ready();
+    this.fetch_connections_new();
+  },
+  computed: {
+    pending_connections: function() {
+      return Object.values(this.connections_new).filter(
+        conn => "state" in conn && conn.state == 'pending'
+      );
+    },
+    failed_connections: function() {
+      return Object.values(this.connections_new).filter(
+        conn => "state" in conn && conn.state === "error"
+      );
+    }
+  },
+  methods: {
+    update_connection: function(form) {
+      let query_msg = {
+        "@type": metadata.types.update,
+        "connection_id": form.connection_id,
+        "label": form.label,
+        "role": form.role,
+      };
+      this.send_message(query_msg);
+    },
+    delete_connection: function(connection) {
+      let query_msg = {
+        "@type": metadata.types.delete,
+        "connection_id": connection.connection_id,
+      };
+      this.send_message(query_msg);
+    },
+    recieve_invitation: function() {
+      let receive_invite_msg = {
+        "@type": metadata.types.receive_invitation,
+        "invitation": this.invitation,
+        "accept": "auto"
+      };
+      this.send_message(receive_invite_msg);
+      this.invitation = "";
+      setTimeout(() => {
+        return this.fetch_connections_new();
+      }, 4000);
+    },
+  },
+}
+</script>
